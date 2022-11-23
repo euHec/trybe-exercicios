b3f3c95442e3e327a861d1b4c924f037fa493afe
euHec <hectorsouzass@gmail.com>
2022-11-23 20:21:27 -0300

    Primeira etapa da atividade 1 concluída

---
+++ Fundamentos/secao-4-introducao-javascript/dia-1-primeiros-passos/script3.js
@@ -0,0 +1,21 @@
+// Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+const number1 = 10;
+const number2 = 15;
+
+let adicao = number1 + number2;
+let subtracao = number1 - number2;
+let multiplicacao = number1 * number2;
+let divisao = number1 / number2;
+let modulo = number1 % number2;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
