f46b8b65c448de54ddc18ff52511924b27f4121b
euHec <hectorsouzass@gmail.com>
2022-11-30 02:12:44 -0300

    Resolução do ponto 4 e 5

--- Fundamentos/secao-4-introducao-javascript/dia-2-array-loop-for/script.js
+++ Fundamentos/secao-4-introducao-javascript/dia-2-array-loop-for/script.js
@@ -24,3 +24,27 @@ let avarege = sum /numbers.length;
 
 console.log(numbers.length);
 console.log(avarege);
+
+console.log('---------------------');
+console.log('atividade 1 - ponto 4');
+
+if (avarege > 20) {
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor ou igual a 20');
+}
+
+console.log('---------------------');
+console.log('atividade 1 - ponto 5');
+
+    let a = numbers[0];
+
+    for (let x = 1; x < numbers.length; x+=1) {    
+        
+        if (a < numbers[x]) {
+            a = numbers[x]
+        }
+    }
+
+
+console.log(a);
