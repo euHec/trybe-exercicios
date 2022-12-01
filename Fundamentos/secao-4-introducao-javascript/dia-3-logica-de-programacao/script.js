da7251cb2f8c12d336cd69b0d1f72a24a9f47442
euHec <hectorsouzass@gmail.com>
2022-12-01 00:28:26 -0300

    Resolução do ponto 3 da atividade

--- Fundamentos/secao-4-introducao-javascript/dia-3-logica-de-programacao/script.js
+++ Fundamentos/secao-4-introducao-javascript/dia-3-logica-de-programacao/script.js
@@ -1,28 +1,54 @@
+// console.log('-----------------');
+// console.log('Ponto 1');
+// 
+// let number = 1;
+// 
+// if (number > 1){
+    // for (let i = number - 1; i > 1; i-= 1) {
+        // number *= i;
+    // }
+// }
+// 
+// console.log(number);
+// 
+// console.log('-----------------');
+// console.log('Ponto 2');
+// 
+// let letter = 'banana';
+// let reverse = "";
+// 
+// for (let index = 0; index < letter.length; index+= 1) {
+    // reverse += letter[letter.length -1 - index];
+    // 
+// }
+// 
+// console.log(reverse);
+// 
 console.log('-----------------');
-console.log('Ponto 1');
+console.log('Ponto 3');
 
-let number = 1;
+// dúvida a ser tirada
 
-if (number > 1){
-    for (let i = number - 1; i > 1; i-= 1) {
-        number *= i;
-    }
-}
+let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-console.log(number);
+let heigtherWord = array[0].length;
+let smallerWord = array[0].length;
 
-console.log('-----------------');
-console.log('Ponto 2');
-
-let letter = 'banana';
-let reverse = "";
-
-for (let index = 0; index < letter.length; index+= 1) {
-    reverse += letter[letter.length -1 - index];
+for (let index = 1; index < array.length; index+=1) {
     
+    if (array[index].length > heigtherWord){
+        heigtherWord = array[index];
+    }
+       
 }
 
-console.log(reverse);
+for (let index = 1; index < array.length; index+=1) {
+    
+    if (array[index].length < smallerWord){
+        smallerWord = array[index];
+    }
+       
+}
 
-console.log('-----------------');
-console.log('Ponto 2');
+console.log('A maior palavra é: ' + heigtherWord + ' que tem ' + heigtherWord.length + ' palavras');
+console.log('A menor palavra é: ' + smallerWord + ' que tem ' + smallerWord.length + ' palavras');
