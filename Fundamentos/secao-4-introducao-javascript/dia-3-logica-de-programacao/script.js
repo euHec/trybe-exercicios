391e416f7e8dc47bd13a93df97cd932b7179a5db
euHec <hectorsouzass@gmail.com>
2022-12-02 23:34:54 -0300

    Resolução do exercício bonus 2

--- Fundamentos/secao-4-introducao-javascript/dia-3-logica-de-programacao/script.js
+++ Fundamentos/secao-4-introducao-javascript/dia-3-logica-de-programacao/script.js
@@ -24,31 +24,55 @@
 // 
 // console.log(reverse);
 // 
+// console.log('-----------------');
+// console.log('Ponto 3');
+
+// // dúvida a ser tirada
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let heigtherWord = array[0].length;
+// let smallerWord = array[0].length;
+
+// for (let index = 1; index < array.length; index+=1) {
+    
+//     if (array[index].length > heigtherWord){
+//         heigtherWord = array[index];
+//     }
+       
+// }
+
+// for (let index = 1; index < array.length; index+=1) {
+    
+//     if (array[index].length < smallerWord){
+//         smallerWord = array[index];
+//     }
+       
+// }
+
+// console.log('A maior palavra é: ' + heigtherWord + ' que tem ' + heigtherWord.length + ' palavras');
+// console.log('A menor palavra é: ' + smallerWord + ' que tem ' + smallerWord.length + ' palavras');
+
+// console.log('-----------------');
+// console.log('Ponto 4');
+
+// let numberP = 5;
+
+// if (numberP % numberP === 0 && numberP % 1 === 0){
+//     console.log('O número ' + numberP + ' é um número primo');
+// }
+        //não consegui realizar a resolução
+
 console.log('-----------------');
-console.log('Ponto 3');
+console.log('Exercicio bônus 1');
 
-// dúvida a ser tirada
+console.log('-----------------');
+console.log('Exercicio bônus 2');
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
+const n = 5;
+let print = '';
 
-let heigtherWord = array[0].length;
-let smallerWord = array[0].length;
-
-for (let index = 1; index < array.length; index+=1) {
-    
-    if (array[index].length > heigtherWord){
-        heigtherWord = array[index];
+    for (let x = 0; x < n; x+=1) {
+        print += '*';
+        console.log(print);
     }
-       
-}
-
-for (let index = 1; index < array.length; index+=1) {
-    
-    if (array[index].length < smallerWord){
-        smallerWord = array[index];
-    }
-       
-}
-
-console.log('A maior palavra é: ' + heigtherWord + ' que tem ' + heigtherWord.length + ' palavras');
-console.log('A menor palavra é: ' + smallerWord + ' que tem ' + smallerWord.length + ' palavras');
